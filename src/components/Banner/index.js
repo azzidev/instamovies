2c11423bcd56103a6ba0728c35331b8dc2b4a1ce
Matheus Azzi <mazzic.dev@gmail.com>
2024-03-13 13:18:01 -0300

    set style banner and fix style header

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -3,6 +3,10 @@ import styles from "./Banner.module.css";
 function Banner(){
     return (
         <div className={styles.banner}>
+            <div className="background-overlay">
+                <span>Dois doidões em Harvard</span>
+                <a>Assistir</a>
+            </div>
         </div>
     );
 }
